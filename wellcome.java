a8b014fa05f0362e7103101b504dc33cffde3059
rishika251 <rishikasadineni@gmail.com>
2022-11-09 10:22:22 +0530

    Added:Wellcome

---
+++ wellcome.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day4;
+
+public class wellcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Wellcome to Employeewage problems");
+	}
+
+}
